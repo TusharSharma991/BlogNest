56742d89108e0cbe725267aaa5ba826c1b8da9f7
TusharSharma991 <tusharshts27601@gmail.com>
2025-05-07 11:23:12 +0530

    .

--- frontend/src/environments/environemnt.ts
+++ frontend/src/environments/environemnt.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://blognest-1-5ysh.onrender.com/'
-    apiUrl: 'http://localhost:3000/'
+    apiUrl: 'https://blognest-1-5ysh.onrender.com/'
+    // apiUrl: 'http://localhost:3000/'
 };
